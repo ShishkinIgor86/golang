cf45e9380ec083eed56d6e629ff6575e28365e2a
Игорь Валерьевич Шишкин <129088534+ShishkinIgor86@users.noreply.github.com>
2024-03-09 18:53:32 +0500

    Update seminar1.go

--- seminar1/seminar1.go
+++ seminar1/seminar1.go
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package main
 
 import (
@@ -35,41 +34,3 @@ func main() {
 	fmt.Printf("extension: %s\n", fileExt)
 
 }
-=======
-package main
-
-import (
-	"fmt"
-	"log"
-	"os"
-    "strings"
-)
-
-func main() {
-	if len(os.Args) < 2 {
-		log.Fatal("Укажите полный путь до файла вторым аргументом")
-	}
-
-	filePth := os.Args[1]
-
-	var fileName, fileExt string
-	// Напишите код, который выведет следующее
-	pointIndex := strings.LastIndex(filePth,".")
-	if pointIndex >= 0{
-		fileName = filePth[strings.LastIndex(filePth,"/")+1: strings.LastIndex(filePth,".")]
-		fileExt = filePth[pointIndex+1:]
-		} else {
-			fileName = filePth[strings.LastIndex(filePth,"/")+1:]
-			fileExt = ""
-		}
-	// filename: <name>
-	// extension: <extension>
-
-	// Подсказка. Возможно вам понадобится функция strings.LastIndex
-	// Для проверки своего решения используйте функции filepath.Base() filepath.Ext(
-	// ) Они могут помочь для проверки решения
-    fmt.Printf("filename: %s\n", fileName)
-	fmt.Printf("extension: %s\n", fileExt)
-
-}
->>>>>>> 92a457b410621416f13840c049c20741829bb85e
